58f36a6ff98fdb5152ab2eeaf3e4d4cec8ea12b6
MNGARCIA085 <mngarcia@mail.antel.com.uy>
2026-08-31 16:26:21 -0300

    separate training and tuning scripts

---
+++ scripts/train.py
@@ -0,0 +1,63 @@
+import hydra
+from hydra.utils import to_absolute_path
+from omegaconf import DictConfig
+
+from anomaly_detection.data.data import DataModule
+from anomaly_detection.experiments.experiments import Experiment
+from anomaly_detection.evaluation.evaluator import Evaluator
+from anomaly_detection.infra.logging.mlflow_logger import  MLFlowLogger
+
+
+@hydra.main(config_path="../config", config_name="config", version_base=None)
+def main(cfg):
+
+
+    # ---------Data -----------#
+    # separate from exp. bc of loading in tuning    
+    data = DataModule(
+        to_absolute_path(cfg.data.train_path),
+        to_absolute_path(cfg.data.val_path),
+        to_absolute_path(cfg.data.y_val_path),
+    )
+    X_train, X_val, y_val = data.load()
+    
+
+    model_type = cfg.model_type.name
+    print(model_type)
+
+
+    #------Experiment--------------#
+    exp = Experiment(
+        model_type=model_type,
+        evaluator=Evaluator(),
+        logger=MLFlowLogger(
+            tracking_db=to_absolute_path(cfg.paths.mlflow_db),
+            artifact_dir=to_absolute_path(cfg.paths.mlflow_artifacts),
+        ),
+    )
+
+
+    metrics = exp.run(
+        cfg.model_type,
+        X_train,
+        X_val,
+        y_val
+    )
+
+
+
+    print(metrics)    
+
+
+    return
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+# hydra multirun: python -m scripts.train -m model_type=ae,vae
